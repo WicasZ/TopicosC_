bf216599699be6c0f7b547387792102adda40681
WILLIAM HUMBERTO ZI CHI <7028@itescam.edu.mx>
2021-04-23 11:20:18 -0500

    Ejercicio4

---
+++ Menu/Ejercicio4/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace Ejercicio4
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.text_in = new System.Windows.Forms.TextBox();
+            this.text_out = new System.Windows.Forms.TextBox();
+            this.btn_aceptar = new System.Windows.Forms.Button();
+            this.btn_clean = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label1.Location = new System.Drawing.Point(47, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Introduce texto";
+            // 
+            // text_in
+            // 
+            this.text_in.Location = new System.Drawing.Point(158, 20);
+            this.text_in.Name = "text_in";
+            this.text_in.Size = new System.Drawing.Size(453, 20);
+            this.text_in.TabIndex = 1;
+            // 
+            // text_out
+            // 
+            this.text_out.Location = new System.Drawing.Point(50, 69);
+            this.text_out.Multiline = true;
+            this.text_out.Name = "text_out";
+            this.text_out.Size = new System.Drawing.Size(674, 318);
+            this.text_out.TabIndex = 2;
+            // 
+            // btn_aceptar
+            // 
+            this.btn_aceptar.Location = new System.Drawing.Point(647, 25);
+            this.btn_aceptar.Name = "btn_aceptar";
+            this.btn_aceptar.Size = new System.Drawing.Size(75, 23);
+            this.btn_aceptar.TabIndex = 3;
+            this.btn_aceptar.Text = "Aceptar";
+            this.btn_aceptar.UseVisualStyleBackColor = true;
+            this.btn_aceptar.Click += new System.EventHandler(this.btn_aceptar_Click);
+            // 
+            // btn_clean
+            // 
+            this.btn_clean.Location = new System.Drawing.Point(649, 404);
+            this.btn_clean.Name = "btn_clean";
+            this.btn_clean.Size = new System.Drawing.Size(75, 23);
+            this.btn_clean.TabIndex = 4;
+            this.btn_clean.Text = "Limpiar";
+            this.btn_clean.UseVisualStyleBackColor = true;
+            this.btn_clean.Click += new System.EventHandler(this.btn_clean_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_clean);
+            this.Controls.Add(this.btn_aceptar);
+            this.Controls.Add(this.text_out);
+            this.Controls.Add(this.text_in);
+            this.Controls.Add(this.label1);
+            this.Name = "Menu";
+            this.Text = "Ventana";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox text_in;
+        private System.Windows.Forms.TextBox text_out;
+        private System.Windows.Forms.Button btn_aceptar;
+        private System.Windows.Forms.Button btn_clean;
+    }
+}
+
---
+++ Menu/Ejercicio4/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio4
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_aceptar_Click(object sender, EventArgs e)
+        {
+            String texto = text_in.Text;
+            text_out.AppendText(texto + "\r\n");
+
+        }
+
+        private void btn_clean_Click(object sender, EventArgs e)
+        {
+            text_in.Text = "";
+            text_out.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
